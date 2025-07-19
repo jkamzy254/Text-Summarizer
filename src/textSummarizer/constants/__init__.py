bc3f07f94b172c71c8b7375b177891aa3fed220b
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2025-07-19 15:38:05 +1000

    Commit 5: Stage 3 - Data Transformation NB Added

--- src/textSummarizer/constants/__init__.py
+++ src/textSummarizer/constants/__init__.py
@@ -1,5 +1,4 @@
 from pathlib import Path
 
-ROOT_DIR = Path(__file__).resolve().parents[3]  # Go up from 'Proj/constants/__init__.py' to project root
-CONFIG_FILE_PATH = ROOT_DIR / "config" / "config.yaml"
-PARAMS_FILE_PATH = ROOT_DIR / "params.yaml"
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
